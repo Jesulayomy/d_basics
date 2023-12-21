a3a2e3846f5cb46081f50444bb87a86cd56a2c60
Jesulayomy <jesulayomi081@gmail.com>
2023-12-21 08:53:32 +0100

    [Tut]: PUT and DELETE methods

--- drinks/drinks/views.py
+++ drinks/drinks/views.py
@@ -30,11 +30,17 @@ def drink_details(request, id):
         return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         if drink:
-            serializer = DrinkSerializer(drink)
+            serializer = DrinkSerializer(drink, data=request.data)
             return Response(serializer.data)
         else:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
     elif request.method == 'PUT':
-        pass
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
-        pass
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

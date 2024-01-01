8739de9f29c8223ccc01efc337be7aeac56e8e42
Jesulayomy <jesulayomi081@gmail.com>
2024-01-01 09:38:26 +0100

    [Tut]: Testing in Django with TestCase

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -25,7 +26,9 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self) -> QuerySet[Any]:
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by("-pub_date")[:5]
 
 
 def counter(request) -> HttpResponse:
@@ -36,6 +39,10 @@ def counter(request) -> HttpResponse:
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """ excludes questions not yet published """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):

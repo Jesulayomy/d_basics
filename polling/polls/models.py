0650242731a087d46ed93bc71e4687267bb63d9c
Jesulayomy <jesulayomi081@gmail.com>
2024-01-07 18:57:57 +0100

    [Model]: Implemented Answer model to highlight correct answer

--- polling/polls/models.py
+++ polling/polls/models.py
@@ -38,3 +38,18 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Answer(models.Model):
+    """ Represents the answer to a question """
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+    choice = models.ForeignKey(
+        Choice,
+        on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return f"{self.choice.choice_text}"
